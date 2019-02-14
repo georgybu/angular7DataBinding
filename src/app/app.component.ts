3e1bcb39943ad524ff6cdc5deb786678d21f7823
Georgy Bunin <georgyb@mprest.com>
2019-02-14 14:06:37 +0200

    create compnoents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,64 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+    selector: 'app-root',
+    template: `
+        <h1>Angular 7.2.0</h1>
+        <h2>Works with object</h2>
+        <pre>{{ user | json }}</pre>
+        <app-user [user]="user"></app-user>
+        <app-user2 [(name)]="user.name" [age]="user.age"></app-user2>
+
+        <!--<hr/>-->
+        <!--<h2>Works with primitives</h2>-->
+        <!--<pre>{{ name | json }}</pre>-->
+        <!--<pre>{{ age | json }}</pre>-->
+        <!--&lt;!&ndash;<app-user [user]="{name: name, age: age}"></app-user>&ndash;&gt;-->
+        <!--&lt;!&ndash;<app-user2 [name]="name" [age]="age"></app-user2>&ndash;&gt;-->
+    `,
+    styles: []
 })
 export class AppComponent {
-  title = 'myApp';
+    user = {
+        name: 'Georgy',
+        age: 30
+    };
+
+    name = 'Iliya';
+    age = 14;
+
+    // Bonus
+    // ngDoCheck() {
+    //     console.log(+new Date(), `change detector was running`);
+    // }
+}
+
+@Component({
+    selector: 'app-user',
+    template: `
+        <input type="text" [(ngModel)]="user.name"/>
+        <input type="number" [(ngModel)]="user.age"/>
+        <pre>{{ user | json }}</pre>
+    `,
+    styles: []
+})
+export class UserComponent {
+    @Input() user;
+}
+
+@Component({
+    selector: 'app-user2',
+    template: `
+        <input type="text" [(ngModel)]="name" (input)="nameChange.emit($event.target.value)"/>
+        <input type="number" [(ngModel)]="age"/>
+        <pre>{{ name | json }}</pre>
+        <pre>{{ age | json }}</pre>
+    `,
+    styles: []
+})
+export class User2Component {
+    @Input() name;
+    @Input() age;
+    @Output() nameChange = new EventEmitter();
+
 }
